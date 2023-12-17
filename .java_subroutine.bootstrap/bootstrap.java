763ce9491724b178d72a2ee61e24bfc42788d61a
M1chaelGuerrer0 <michael.guerrero.013@my.csun.edu>
2023-12-17 03:18:27 -0800

    copying files

---
+++ .java_subroutine.bootstrap/bootstrap.java
@@ -0,0 +1,96 @@
+class checksum {
+   static MIPS_OS_Interface mips = new MIPS_OS_Interface();
+
+	public static int checksum() {
+		// $t0 : i
+		// $t1 : sum
+		// $t2 : header_checksum
+		// $t3 : max_int
+		// $t4 : quotient
+		// $t5 : remainder
+		// $t6 : checksum
+		// $t7 : temp
+		// $t8 : mips.retval()
+		// $v0 : result
+		int $t0;
+		int $t1;
+		$t1 = 0;
+		int $t2;
+		$t2 = 0;
+		final int $t3;
+		$t3 = 255;
+		int $t4;
+		$t4 = 0;
+		int $t5;
+		$t5 = 0;
+		int $t6;
+		$t6 = 0;
+		int $t7;
+		$t7 = 0;
+		int $t8;
+		$t8 = 0;
+		int $v0;
+		$v0 = 0;
+
+init:	;
+		$t0 = 0;
+loop:	for(;$t0 < 10;) {
+body:		;
+			mips.read_d();
+			$t8 = mips.retval();
+			$t1 += $t8;
+ifeq:	   		if($t0 == 5) {
+con:			;
+	   			$t2 = $t8;
+	   			// goto end
+	   		}
+alt:		;
+			// goto end
+end:		;
+			// goto next
+next:		;	
+			$t0++;	
+			continue loop;	
+		}
+done:	;		
+		
+		$t1 -= $t2;
+		$t7 = $t3 + 1;
+		$t4 = $t1 / ($t7);
+		$t5 = $t1 % ($t7);
+		$t7 = $t4 + $t5;
+		$t6 = $t3 - ($t7);
+
+equals:	if($t6 == $t2) {
+is:			;
+			$v0 = 0;
+			return $v0;
+		}
+		else {
+isnt:		;	
+			$v0 = $t6;
+			return $v0;
+		}
+	}
+
+// Task 1 complete: Tue Oct 10 21:59:25 PDT 2023
+
+// Task 2 complete: Tue Oct 10 22:24:47 PDT 2023
+
+
+  public static void main(String[] args) {
+  
+    int index;
+
+    int $v0 = checksum();
+
+    // Augment this code based upon desired output type
+
+    mips.print_ci('\n');   // Print extra '\n' in case the users
+                           // last line does not include the 
+                           // proforma '\n'.
+    mips.print_d($v0);
+    mips.print_ci('\n');
+    return;
+  }
+}
---
+++ checksum.s
@@ -0,0 +1,86 @@
+.text
+	.globl checksum
+		.macro read_d()
+        	nop                     # Reads from stdin, a decimal (%d) number
+        	li $v0, 5
+        	syscall                 
+        	nop                     # The value is now in $v0
+		.end_macro
+			# $t0 : i
+			# $t1 : sum
+			# $t2 : header_checksum
+			# $t3 : max_int
+			# $t4 : quotient
+			# $t5 : remainder
+			# $t6 : checksum
+			# $t7 : temp
+			# $t8 : mips.retval()
+			# $v0 : result
+
+checksum:	nop							#	public static int checksum() {		
+										#		int $t0;
+										#		int $t1;
+			li $t1, 0					#		$t1 = 0;
+										#		int $t2;
+			li $t2, 0					#		$t2 = 0;
+										#		final int $t3;
+			li $t3, 255					#		$t3 = 255;
+										#		int $t4;
+			li $t4, 0					#		$t4 = 0;
+										#		int $t5;
+			li $t5, 0					#		$t5 = 0;
+										#		int $t6;
+			li $t6, 0					#		$t6 = 0;
+										#		int $t7;
+			li $t7, 0					#		$t7 = 0;
+										#		int $t8;
+			li $t8, 0					#		$t8 = 0;
+										#		int $v0;
+			li $v0, 0 					#		$v0 = 0;
+init:		nop							#		;
+			li $t0, 0					#		$t0 = 0;
+loop:		bge $t0, 10, done			#		for(;$t0 < 10;) {
+body:			nop						#			;
+				read_d()				#			mips.read_d();
+				move $t8, $v0			# 			$t8 = mips.retval();
+				add $t1, $t1, $t8		#			$t1 += $t8;
+ifeq:				bne $t0, 5, alt	 	#			if($t0 == 5) {
+cons:				nop				   	#				;
+					move $t2, $t8	   	#				$t2 = $t8;
+					b end				#				// goto end	
+										#			} 	
+alt:			nop						#			;
+					b end 				#			// goto end
+end:			nop						#			;
+					b next				#			// goto next
+next:			nop						#			;	
+				addi $t0, $t0, 1 		#			$t0++;	
+				b loop					#			continue loop;	
+										#		}
+done:		nop							#		;		
+			sub	$t1, $t1, $t2			#		$t1 -= $t2;
+			addi $t7, $t3, 1			#		$t7 = $t3 + 1;
+			div $t1, $t7 				#		$t4 = $t1 / ($t7);
+			mflo $t4					
+			div $t1, $t7				#		$t5 = $t1 % ($t7);	
+			mfhi $t5	
+			add $t7, $t4, $t5			#		$t7 = $t4 + $t5
+			sub $t6, $t3, $t7			#		$t6 = $t3 - ($t7);
+equal: 		bne $t6, $t2, isnt			#		if($t6 == $t2) {
+is:				nop						#			;
+				li $v0, 0				#			$v0 = 0;
+				jr $ra					#			return $v0;
+				
+										#		}
+isnt:		nop							#		else {
+				move $v0, $t6			#			$v0 = $t6;
+				jr $ra 					#			return $v0;
+				
+										#		}
+
+# Task 1 complete: Tue Oct 10 21:59:25 PDT 2023
+
+# Task 2 complete: Tue Oct 10 22:24:47 PDT 2023
+
+
+# Task 3 complete: Thu Oct 12 10:59:15 PDT 2023
